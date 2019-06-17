ab5a6c51ec0d5e8086400b30a5a5746b18a19491
jvanner <j.vanner@rogers.com>
2019-06-16 20:28:20 -0400

    Cleanup

--- GimmePizza/Program.cs
+++ GimmePizza/Program.cs
@@ -25,23 +25,6 @@ namespace GimmePizza
             order1.item_list.Add(item2);
 
             order1.printOrder();
-            //string[] foodFileItems = {};
-            //List<FoodItem> food_list = new List<FoodItem>();
-            //try
-            //{
-            //    foodFileItems = File.ReadAllLines("C:\\Users\\Joshua\\Documents\\GitHub\\GimmePizza\\GimmePizza\\FoodData\\food_data.txt");
-            //}
-            //catch (FileNotFoundException)
-            //{
-            //    Console.WriteLine("HALP");
-            //    Environment.Exit(0);
-            //}
-            //foreach (string item in foodFileItems)
-            //{
-            //    string[] itemSplit = item.Split(',');
-            //    FoodItem nextItem = new FoodItem(itemSplit[0], itemSplit[1], Convert.ToDouble(itemSplit[2]));
-            //    food_list.Add(nextItem);
-            //}
             List<FoodItem> food_list = FoodItem.ReadFoodData("C:\\Users\\Joshua\\Documents\\GitHub\\GimmePizza\\GimmePizza\\FoodData\\food_data.txt");
             Order order2 = new Order();
             foreach(FoodItem food in food_list)
@@ -51,9 +34,6 @@ namespace GimmePizza
 
             order2.printOrder();
             Console.ReadKey();
-
-            
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
